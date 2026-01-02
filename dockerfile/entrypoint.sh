555b5faf0345084f0c1b917cdda993eb5d558dd5
Y4nareth <andrescalandin@gmail.com>
2026-01-02 13:44:06 +0100

    Version 0.2 - Staticfiles para produccion

--- dockerfile/entrypoint.sh
+++ dockerfile/entrypoint.sh
@@ -3,5 +3,8 @@
 echo "Running migrations"
 python manage.py migrate --noinput
 
+echo "Collecting static files"
+python manage.py collectstatic --noinput
+
 echo "Starting app"
 exec daphne -b 0.0.0.0 -p 8000 first_step_app.asgi:application
